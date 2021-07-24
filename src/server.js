c12db5de99b9055282531d2a7734af0f491b1155
prakash-amit <amit.prakash.se@gmail.com>
2021-07-24 17:21:16 +0530

    add sample get route with param and post route with body

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-
+import bodyParser  from 'body-parser';
 const app = express();
-
+app.use(bodyParser.json())
 app.get('/hello',(req, res) => res.send('Hello'));
-
-app.listen(8000, () => console.log( 'listening to port 8000'));
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
+app.post('/hello',(req, res) => res.send(`Hello ${req.body.name}`));
+app.listen(8080, () => console.log( 'listening to port 8080'));
