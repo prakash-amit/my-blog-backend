86d4fc9b8fb858064ff7b8f9cf8d3e13af3ccf89
prakash-amit <amit.prakash.se@gmail.com>
2021-07-26 10:49:30 +0530

    preparing blog app for release

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser  from 'body-parser';
 import { MongoClient } from 'mongodb';
 import { async } from 'regenerator-runtime';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json())
 
 const withDB = async (operations, res) => {
@@ -61,4 +64,7 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
     }, res);
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
 app.listen(8080, () => console.log( 'listening to port 8080'));
